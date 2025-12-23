7095d6a1cef313e1fd571ac785447c3fdce81672
Minh Tran <trcongminh.261002@gmail.com>
2025-12-23 19:11:00 +0700

    feat: refactor page component, update providers, and add Convex integration

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,12 +1,11 @@
-import { Button } from "@workspace/ui/components/button";
+"use client";
 
-export default function Page() {
-  return (
-    <div className="flex items-center justify-center min-h-svh">
-      <div className="flex flex-col items-center justify-center gap-4">
-        <h1 className="text-2xl font-bold">Hello World Web</h1>
-        <Button size="sm">Button</Button>
-      </div>
-    </div>
-  );
-}
+import React, { use } from "react";
+import { useQuery } from "convex/react";
+import { api } from "@workspace/backend/_generated/api";
+const Page = () => {
+  const users = useQuery(api.users.getMany);
+  return <div>{JSON.stringify(users, null, 2)}</div>;
+};
+
+export default Page;

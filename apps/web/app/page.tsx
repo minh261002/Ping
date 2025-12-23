3a21e793d06d7c4071f1c7865e400bce2c325a7f
Minh Tran <trcongminh.261002@gmail.com>
2025-12-23 19:42:27 +0700

    feat: implement authentication layout and pages with Clerk integration

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,11 +1,28 @@
 "use client";
 
-import React, { use } from "react";
-import { useQuery } from "convex/react";
+import React from "react";
+import { Unauthenticated, useMutation, useQuery } from "convex/react";
 import { api } from "@workspace/backend/_generated/api";
+import { Button } from "@workspace/ui/components/button";
+import { Authenticated } from "convex/react";
+import { SignInButton, UserButton } from "@clerk/nextjs";
+
 const Page = () => {
   const users = useQuery(api.users.getMany);
-  return <div>{JSON.stringify(users, null, 2)}</div>;
+  const addUser = useMutation(api.users.ad);
+  return (
+    <div>
+      <Authenticated>
+        {JSON.stringify(users, null, 2)}
+        <Button onClick={() => addUser()}>Create User</Button>
+        <UserButton />
+      </Authenticated>
+      <Unauthenticated>
+        <div>Please sign in to view users.</div>
+        <SignInButton>Sign In</SignInButton>
+      </Unauthenticated>
+    </div>
+  );
 };
 
 export default Page;

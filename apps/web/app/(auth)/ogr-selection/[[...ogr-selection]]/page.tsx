b74c1862f72fdc51aa62ed11bc6a5ad6e7bf913d
Minh Tran <trcongminh.261002@gmail.com>
2025-12-23 20:18:36 +0700

    feat: implement organization selection view and guard for authentication flow

---
+++ apps/web/app/(auth)/ogr-selection/[[...ogr-selection]]/page.tsx
@@ -0,0 +1,8 @@
+import OgrSelectionView from "@/modules/auth/ui/views/ogr-selection-view";
+import React from "react";
+
+const page = () => {
+  return <OgrSelectionView />;
+};
+
+export default page;
